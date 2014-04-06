4cb9625d3ab6c6053a613d9b23b77a7582b462b7
Martin Muñoz <im.mmun@gmail.com>
2014-04-06 00:54:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Broccoli's ES6 Module Transpiler
+
+## Usage
+
+```javascript
+var transpileES6 = require('broccoli-es6-module-transpiler');
+
+var transpiledLib = transpileES6(lib, {
+  moduleName: function(filePath) {
+    return filePath.replace(/.js$/, '');
+  }
+});
+```
+
+## Documentation
+
+### `transpileES6(inputTree, options)`
+
+###### `options.type`: **{String}** The type of module to transpile to.
+Possible values: `amd`, `cjs`, `yui` or `globals`.
+Default: `amd`.
+
+###### `options.moduleName`: **{Function, String, null}** The module name that is passed to the transpiler for each file.
+If `moduleName` is `null` an anonymous module will be generated.
+If `moduleName` is a string it will be passed directly the transpiler for each file.
+If `moduleName` is a function it will be called with the path of the current file as its sole argument.
+The return value of the function will be passed to the transpiler. 
+Default: `null`.
+
+###### `options.options`: **{Function, Object, null}** The options that are passed to the transpiler for each file.
+If `options` is `null` no options will be passed.
+If `options` is an object it will be passed directly the transpiler for each file.
+If `options` is a function it will be called with the path of the current file as its sole argument.
+The return value of the function will be passed to the transpiler. 
+Default: `null`.
---
+++ index.js
@@ -0,0 +1,53 @@
+var Filter = require('broccoli-filter');
+var Compiler = require('es6-module-transpiler').Compiler;
+
+module.exports = TranspilerFilter;
+
+TranspilerFilter.prototype = Object.create(Filter.prototype);;
+TranspilerFilter.prototype.constructor = TranspilerFilter;
+
+function TranspilerFilter(inputTree, options) {
+  if (!(this instanceof TranspilerFilter)) {
+    return new TranspilerFilter(inputTree, options);
+  }
+  options = options || {};
+  this.inputTree = inputTree;
+  this.type = options.type || 'amd';
+  this.moduleName = options.moduleName;
+  this.transpilerOptions = options.options;
+}
+
+TranspilerFilter.prototype.extensions = ['js'];
+TranspilerFilter.prototype.targetExtension = 'js';
+
+TranspilerFilter.prototype.getModuleName = function(filePath) {
+  if (typeof this.moduleName === 'function') {
+    return this.moduleName(filePath);
+  } else {
+    return this.moduleName;
+  }
+};
+
+TranspilerFilter.prototype.getTranspilerOptions = function(filePath) {
+  if (typeof this.transpilerOptions === 'function') {
+    return this.transpilerOptions(filePath);
+  } else {
+    return this.transpilerOptions;
+  }
+};
+
+TranspilerFilter.prototype.processString = function (fileContents, filePath) {
+  var name = this.getModuleName(filePath);
+  var options = this.getTranspilerOptions(filePath);
+
+  var compiler = new Compiler(fileContents, name, options);
+
+  return compiler[transpilerMethods[this.type]]();
+};
+
+var transpilerMethods = {
+  'amd': 'toAMD',
+  'yui': 'toYUI',
+  'cjs': 'toCJS',
+  'globals': 'toGlobals'
+};
